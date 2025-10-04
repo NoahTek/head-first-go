6439fbcf7b08a54d5fcf1ff1c57414358e9c5084
Manu <49262925+NoahTek@users.noreply.github.com>
2025-10-04 14:25:59 +0530

    Created package magazine with type structs and embedded structs.

---
+++ magazine/magazine.go
@@ -0,0 +1,22 @@
+// Package magazine defines several structs
+package magazine
+
+type Address struct {
+	Street     string
+	City       string
+	State      string
+	PostalCode string
+}
+
+type Subscriber struct {
+	Name   string
+	Rate   float64
+	Active bool
+	Address
+}
+
+type Employee struct {
+	Name   string
+	Salary float64
+	Address
+}
