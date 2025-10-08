44dd293a565a9001a2465cca8d7ba70be52c1eb8
Manu <49262925+NoahTek@users.noreply.github.com>
2025-10-08 19:31:31 +0530

    Ch13: Programs sleep_channel & url were created to learn to use goroutines & channels.

---
+++ url/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func responseSize(url string, channel chan int) {
+	fmt.Println("Getting ", url)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	channel <- len(body)
+}
+
+func main() {
+	sizes := make(chan int)
+	go responseSize("https://golang.org/", sizes)
+	go responseSize("https://golang.org/doc", sizes)
+	go responseSize("https://example.com", sizes)
+	fmt.Println(<-sizes)
+	fmt.Println(<-sizes)
+	fmt.Println(<-sizes)
+}
