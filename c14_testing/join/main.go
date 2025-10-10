f1aa34e041b2d880803cc1faf6a36c472245822f
Manu <49262925+NoahTek@users.noreply.github.com>
2025-10-10 18:42:06 +0530

    C14: Learning test driven development.
    Added cases for one element and two elements.

--- c14_testing/join/main.go
+++ c14_testing/join/main.go
@@ -11,4 +11,6 @@ func main() {
 	fmt.Println("A photo of", prose.JoinWithComma(phrases))
 	phrases = []string{"my parents", "a rodeo clown", "a prize bull"}
 	fmt.Println("A photo of", prose.JoinWithComma(phrases))
+	phrases = []string{"my parents"}
+	fmt.Println("A photo of", prose.JoinWithComma(phrases))
 }
